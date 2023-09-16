1edab9654b73e5013559106d0adaf3f77800d6f6
Kaveesha <kalshandhanasekara@gmail.com>
2023-09-16 13:10:59 +0530

    change router path

--- router/user.js
+++ router/user.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const update = require('../controller/update')
 const getWeatherData = require('../controller/getWeatherData')
+const getAll = require('../controller/getAll');
+const insertWeather = require('../controller/insertWeather');
 
 router.put('/update', update);
+router.get('/insertWeather', insertWeather);
 router.get('/getWeatherData', getWeatherData);
+router.get('/getAll', getAll);
 
 module.exports = router;
