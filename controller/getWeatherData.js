e80c9ea11ae2cc912f478231420a7cc8e3dfd71e
Kaveesha <kalshandhanasekara@gmail.com>
2023-09-19 21:10:29 +0530

    some changes

--- controller/getWeatherData.js
+++ controller/getWeatherData.js
@@ -5,8 +5,9 @@ async function getWeatherData(req, res){
         const {id, date} = req.query;
         const user = await userModel.findById(id);
         const weatherData = user.weatherData.filter(entry => entry.date.toISOString().split('T')[0] === date);
-        if (!weatherData) {
-            console.log('Weather data not found for the specified date');
+        if (!weatherData[0]) {
+            console.log('Weather data are not found on this day.');
+            return res.send('Weather data are not found on this day.');
         }
         res.json(weatherData);
     } catch (e) {
